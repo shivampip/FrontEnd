dc219bd706b8bf200fa784a9ecc43d662fa846ce
shivampip <shivam301296@gmail.com>
2019-12-16 17:27:01 +0530

    updated

---
+++ jQueryDemo/one_two/main.js
@@ -0,0 +1,26 @@
+// $("h3").hide();
+// $("h3#myid").hide();
+// $("h3.myclass").hide();
+
+// <div><p>ram</p></div>
+// $("div p").css("color", "green");
+
+// ul with id mylist, ka first li
+// first, last, even, odd, nth-child(3n) etc
+//$("ul#mylist li:first").hide();
+
+
+//<input type="button" value="Click Me">
+//$(":button").hide();
+//$(":submit").hide();
+//$(":text").hide();
+
+
+//Change all element which has href attribute
+// $("[href]").css("color", "purple");
+
+//exact
+// $("a[href='https://www.google.com/']").hide();
+
+//Everything :)
+// $("*").hide();
